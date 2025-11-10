15616d5bd0f5309223f5ffec0c346030f5554f30
Saikat Sardar <saikatsardar5@gmail.com>
2025-11-10 15:58:58 +0530

    fix: importing server auth into the client bundle

--- src/lib/betterAuth/auth-client.ts
+++ src/lib/betterAuth/auth-client.ts
@@ -1,10 +1,11 @@
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { clientEnv } from "../env/clientEnv";
-import { auth } from "./auth";
+
+type AuthInstance = typeof import("./auth").auth;
 
 export const authClient = createAuthClient({
 	/** The base URL of the server (optional if you're using the same domain) */
 	baseURL: clientEnv.NEXT_PUBLIC_BETTER_AUTH_URL,
-	plugins: [inferAdditionalFields<typeof auth>()],
+	plugins: [inferAdditionalFields<AuthInstance>()],
 });
